dff84beeb80ed4ecb2efabf1d886773812bf3a85
gazer33 <117957218+gazer33@users.noreply.github.com>
2023-04-25 20:22:36 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const { redirect } = require("next/dist/server/api-utils");
+
+module.exports ={
+  reactStricMode: true,
+   images: {
+      
+    domains: ['placeimg.com', 'picsum.photos',
+     'escuelajs.co', 'cdn.shopify.com', 'i.imgur.com',
+      'encrypted-tbn0.gstatic.com', 'i.blogs.es', 'imgur.com',
+       'us.123rf.com','i.stack.imgur.com','avatars.mds.yandex.net',
+       'www.motor.com.co','api.lorem.space','empresas.blogthinkbig.com',
+       'img.freepik.com','images.unsplash.com','www.woodenson.co','api.escuelajs.co'],
+  },
+}
